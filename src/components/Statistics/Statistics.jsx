4570703aa70df7f9db263d513b293341ac0c027a
rizorno <114782234+rizorno@users.noreply.github.com>
2023-02-04 00:53:45 +0100

    updates

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import css from './statistics.module.scss';
 
 function Statistics({ good, neutral, bad, total, positivePercentage }) {
